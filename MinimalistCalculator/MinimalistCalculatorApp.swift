5ef9dedc6612b04f2357ad0b9182cff6910b6e24
Daniel Garcia <pel_daniel@hotmail.com>
2022-12-08 12:36:40 -0600

    Initial Commit

---
+++ MinimalistCalculator/MinimalistCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalistCalculatorApp.swift
+//  MinimalistCalculator
+//
+//  Created by Daniel Garcia on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MinimalistCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
